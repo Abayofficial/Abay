2d943817ae2486d8bb1bd6f9a5a77337175fbb7d
Abayofficial <iqbalantariksa9@gmail.com>
2022-02-06 08:11:08 +0700

    Abay

---
+++ message/sewabot.js
@@ -0,0 +1,50 @@
+
+exports.dada = (prefix, settings, pushname, ucapanWaktu) => {
+return `${ucapanWaktu} kak ${pushname}
+Berikut adalah list harga untuk sewa botz
+
+*╭─▸*「 𝗟𝗜𝗦𝗧 𝗦𝗘𝗪𝗔 」
+*┴*
+*│▸* Perhari = 1K
+*│▸* PERMINGGU = 5k         
+*│▸* PERBULAN = 7k          
+*│▸* PERMANEN = 10k         
+*┬*
+*╰────────────[*
+
+╔════════════•| ✿ |•═════════════╗
+                                *_PAYMENT_*:
+
+➪ *_GOPAY_*: 087788069091  A/N ABAY STORE
+➪ *_DANA_*: 087788069091 A/N RE** HER**** YAN**
+
+                      *_SERTAKAN BUKTI TF!!_*
+╚════════════•| ✿ |•═════════════╝
+
+𝗡𝗢𝗧𝗘 : 
+
+Bot on 24 jam tapi kadang 
+juga mati klo lgi ada error 
+atau lgi perbaikan bug
+`
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
